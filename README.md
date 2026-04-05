8825b0d1146cb98445dac99dea746c555579483c
t <t.aryurek@student.utwente.nl>
2026-04-05 20:01:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+# STL Implementations and Custom Extensions
---
+++ thread_pool.h
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <execution>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+
+template <std::size_t N>
+  requires(N > 0 && N <= 100)
+class ThreadPool {
+
+public:
+  ThreadPool() {}
+
+  ~ThreadPool() {}
+
+  template <typename _Signature> void addTask(std::function<_Signature> task) {
+    lock();
+    mQueue.push(task);
+    unlock();
+  }
+
+  void dispatch() {
+    std::cout << "dispatched!\n";
+    while (!mInterrupt) {
+      lock();
+      auto it = std::find_if(
+          mThreadPool.begin(), mThreadPool.end(),
+          [](const std::thread &thread) { return !thread.joinable(); });
+      if (it == mThreadPool.end()) {
+        std::clog << "Could not find an empty thread!\n";
+        continue;
+      }
+      if (mQueue.empty()) {
+        std::clog << "\nEmpty!\n";
+        interrupt();
+        unlock();
+        return;
+      }
+      Task<void()> a{mQueue.front()};
+      mQueue.pop();
+      mThreadPool[it - mThreadPool.begin()] =
+          std::move(std::thread([&a]() { a.task(); }));
+      for (std::thread &tr : mThreadPool) {
+        if (tr.joinable()) {
+          tr.join();
+        };
+      }
+      unlock();
+    }
+  }
+
+  void interrupt() { mInterrupt = true; }
+
+  std::size_t poolSize() const { return mThreadPool.size(); }
+
+  std::array<std::thread, N> &pool() { return mThreadPool; }
+
+private:
+  template <typename _Signature> struct Task {
+    Task(std::function<_Signature> f) : task(f), isDone(false) {}
+    std::function<_Signature> task;
+    std::atomic<bool> isDone{false};
+  };
+
+private:
+  void lock() { mLock.lock(); }
+
+  void unlock() { mLock.unlock(); }
+
+  std::mutex mLock;
+  std::array<std::thread, N> mThreadPool;
+  std::queue<std::function<void()>> mQueue{};
+  std::atomic<bool> mInterrupt{false};
+};
